cbfac6e420fc21d0510753f778d51f4951d2708c
root <adebiyibasit11@gmail.com>
2022-04-19 23:50:44 +0000

    First trial

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to scripting lifestyle"
+echo
+echo
+sleep 5
+echo "If you like, I can create a user with a folder for you"
+sleep 2
+echo
+echo
+echo "Would you like to go ahead and create a personal user for you"
+
+
